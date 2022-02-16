6384226f3a238f614150873a74d87105d74b0eaf
Kenechukwu Frank <kenisank1@gmail.com>
2022-02-16 04:00:11 +0100

    Initial

---
+++ ParkyApi/Data/EFContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkyApi.Models;
+
+namespace ParkyApi.Data
+{
+    public class EFContext:DbContext
+    {
+        public EFContext(DbContextOptions<EFContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<NationalParks> NationalParks { get; set; }
+    }
+}
